6fde2714883668cd7614e0667876c60362f2751a
pmengy <936841379@qq.com>
2022-07-10 09:11:43 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/mobile/flexible' // 适配
-import '@/styles/reset.css' // 初始化样式
+import Vue from 'vue';
+import App from './App.vue';
+import '@/assets/mobile/flexible'; // 适配
+import '@/assets/styles/reset.css'; // 初始化样式
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount('#app');
